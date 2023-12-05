b48d33192bd8e6db5091f98cddb91bc22364ab6f
pavva91 <valerio.mattioli@proton.me>
2023-12-05 03:23:24 +0100

    Class with Param Properties - concise way to declare classes

---
+++ src/ts_fundamentals_examples/class-with-param-properties.ts
@@ -0,0 +1,8 @@
+// NOTE: Abbreviated way to declare a class (use this to declare classes)
+class Car {
+    constructor(
+        public make: string,
+        public model: string,
+        public readonly year: number
+    ) { }
+}
